6329590e4c55c181b1f7a817123d5b226f190d6b
Boris <sslonovborisss@gmail.com>
2022-12-11 08:18:46 +0300

    about

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ import Footer from '../components/Footer'
 
 
 
-export default function Home() {
+export default function About() {
     return (
         <div className={styles.container}>
             <Head>
